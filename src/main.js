e564f360f9803cec2cf6d3cd55d495d2cc6a34af
Md. Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2021-02-28 00:17:24 +0600

    disqus comment adding

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@
 import './assets/styles.css'
 
 import DefaultLayout from '~/layouts/Default.vue'
+import VueDisqus from 'vue-disqus'
+
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  Vue.use(VueDisqus)
 }
--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,9 +1,12 @@
 <template>
   <Layout>
-    <h1 v-html="$page.post.title" />
-    <h3>By {{ $page.post.author }} on {{ $page.post.date }}</h3>
-    <g-image :src="$page.post.thumbnail" style="width: 10rem;" />
-    <div v-html="$page.post.content" />
+    <main>
+      <h1 v-html="$page.post.title" />
+      <h3>By {{ $page.post.author }} on {{ $page.post.date }}</h3>
+      <g-image :src="$page.post.thumbnail" style="width: 10rem;" />
+      <div v-html="$page.post.content" />
+    </main>
+    <Disqus shortname="blogportfolio" :identifier="$page.post.title" />
   </Layout>
 </template>
 
