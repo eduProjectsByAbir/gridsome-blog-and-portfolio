d8074d4c57ee91721fbbd1c2fb0a32bcd02304f3
Md. Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2021-02-27 16:31:18 +0600

    project closed for unknown error on node dependinces

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <header>
+    <h2 v-html="$static.metadata.siteName" />
+
+    <nav>
+      <g-link to="/">Home</g-link>
+      <g-link to="/about">About</g-link>
+      <g-link to="/blog">Blog</g-link>
+    </nav>
+  </header>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style lang="scss" scoped>
+header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: calc(100% - 12rem);
+  padding: 0.5rem 6rem;
+  background: $blue;
+  color: $white;
+}
+
+h2 {
+  font-size: 1.4rem;
+  font-weight: 500;
+}
+
+nav a {
+  color: inherit;
+  text-decoration: none;
+  padding: 1rem;
+  font-size: 1.2rem;
+}
+</style>
